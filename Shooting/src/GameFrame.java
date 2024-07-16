6f37be4c01103bdd359ae3e85e5d88f4fa36b3d8
sasahara <zeal23310180@fsgcl.jp>
2024-07-16 12:03:22 +0900

    13-4をやった。

--- Shooting/src/GameFrame.java
+++ Shooting/src/GameFrame.java
@@ -4,6 +4,9 @@ public class GameFrame extends MyFrame {
 	public void run() {
 		GameWorld.player = new Player(100, 300, 0, 0);
 		addKeyListener(GameWorld.player);
+		while(true) {
+			GameWorld.player.x=100;
+			GameWorld.player.y=300;
 		GameWorld.playerBullets = new Vector<PlayerBullet>();
 		GameWorld.enemies = new Vector<Enemy>();
 		GameWorld.enemies.add(new EnemyBase(100, 50, 1, 0));
@@ -30,6 +33,7 @@ public class GameFrame extends MyFrame {
 				}
 			}
 			sleep(0.03);
+		}
 		}
 	}
 	public void moveEnemies() {
